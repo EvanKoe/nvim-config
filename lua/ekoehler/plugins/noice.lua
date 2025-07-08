2088b31a4095ad15e5ee06b4ef645b8151d2a06a
EvanKoe <evan.koehler@epitech.eu>
2025-07-08 23:00:17 +0200

    added Noice

---
+++ lua/ekoehler/plugins/noice.lua
@@ -0,0 +1,21 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+			}
+		},
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			lsp_doc_border = true
+		}
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+	}
+}
